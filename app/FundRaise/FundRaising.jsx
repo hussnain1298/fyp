4006a05067fa6ca53484d9424abab7c889d1dbcf
Amna <amnarazzaq0318@gmail.com>
2025-06-12 17:30:42 +0500

    set

--- app/FundRaise/FundRaising.jsx
+++ app/FundRaise/FundRaising.jsx
@@ -5,7 +5,6 @@ import Slider from "react-slick";
 import { firestore } from "@/lib/firebase";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import FundRaiserCard from "./FundRaiserCard";
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
